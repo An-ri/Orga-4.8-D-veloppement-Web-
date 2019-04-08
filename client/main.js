5743b3042e97b2ae745b56d1eba6378a71072b99
An-ri <46490745+An-ri@users.noreply.github.com>
2019-04-08 17:17:33 +0200

    Add files via upload

--- client/main.js
+++ client/main.js
@@ -1,9 +1,22 @@
-import '../imports/startup/client/routes.js';
-import '../imports/startup/client/accounts-config.js'
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
 
-import '../imports/ui/layouts/layout1.js';
-import '../imports/ui/home.js';
-import '../imports/ui/tasks.js';
-import '../imports/ui/tasks2.js';
-import '../imports/ui/connection.js';
-import '../imports/ui/inscription.js';
+import './main.html';
+
+Template.hello.onCreated(function helloOnCreated() {
+  // counter starts at 0
+  this.counter = new ReactiveVar(0);
+});
+
+Template.hello.helpers({
+  counter() {
+    return Template.instance().counter.get();
+  },
+});
+
+Template.hello.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() + 1);
+  },
+});
